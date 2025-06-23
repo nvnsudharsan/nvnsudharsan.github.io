ccc395780d11feae42b76915d27b1c8d958a3177
Naveen Sudharsan <57706774+nvnsudharsan@users.noreply.github.com>
2025-06-22 22:12:45 -0400

    Update Updates.js

--- src/components/Updates.js
+++ src/components/Updates.js
@@ -8,11 +8,6 @@ const updates = [
     link: 'https://cesium.com/events/cesium-developer-conference/'
   },
   {
-    title: 'Attending S2S Community Workshop: Toward Minimizing Early Model Biases and Errors in S2S Predictions by NOAA',
-    date: 'June 16 - 18, 2025',
-    link: 'https://wpo.noaa.gov/s2s-community-workshop/'
-  },
-  {
     title: 'Resource Person for 3-day tutorial-cum-workshop on "Applications of AI/ML and km-scale Physical Models" by Atria University',
     date: 'June 5 - 7, 2025',
     link: 'https://www.linkedin.com/posts/madhavan-nair-rajeevan-38822526_tutorial-and-workshop-on-artificial-intelligence-activity-7314676934548852736-M8h_?utm_source=share&utm_medium=member_desktop&rcm=ACoAABR4ETcBIo86GV6B3vRJM_ahK6h2WgzFSKU'
