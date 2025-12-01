8ef0103867acd9b7f0c1d409e5875a56df9e591d
Naveen Sudharsan <nvnsudharsan@gmail.com>
2025-12-01 13:57:20 -0600

    Add Chicago Overcast photography entry
    Added a new photography entry for 'Chicago Overcast' with camera details and editing adjustments.

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -136,6 +136,18 @@ const photos = [
     }
   },
     {
+    src: `${process.env.PUBLIC_URL}/Photography/Chicago_overcast.jpg`,
+    title: 'Chicago Overcast',
+    camera: {
+      camera: 'Sony A6400',
+      lens: 'Viltrox 25mm f/1.7',
+      settings: 'f/2.4, 1/125s, ISO 1600'
+    },
+    editing: {
+      adjustments: 'Exposure -0.27, Contrast +45, Highlights +39, Shadows +49, Whites +25, Blacks -49, Saturation +25, Texture +100, Clarity +62, Dehaze +46'
+    }
+  },
+    {
     src: `${process.env.PUBLIC_URL}/Photography/Keerthi_1.jpg`,
     title: 'Portrait4',
     camera: {
